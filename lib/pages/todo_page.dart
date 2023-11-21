602f81be59bb4e38955c765965d1b89a24f2afa6
Gwanghyeon <jesse0221@naver.com>
2023-11-21 17:03:46 +0900

    11-21

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,14 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_v1/providers/providers.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Todos'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+            child: Column(
+              children: [
+                TodoHeader(),
+                CreateTodo(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class TodoHeader extends StatelessWidget {
+  const TodoHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('TODO'),
+        // from ActiveTodoCount, rebuild through watch method
+        Text(
+            '${context.watch<ActiveTodoCount>().state.activeTodoCount} items left'),
+      ],
+    );
+  }
+}
+
+class CreateTodo extends StatefulWidget {
+  const CreateTodo({super.key});
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  final todoTextCont = TextEditingController();
+
+  @override
+  void dispose() {
+    todoTextCont.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: todoTextCont,
+      decoration: InputDecoration(labelText: 'What is your next move?'),
+      onSubmitted: (String? todoDesc) {
+        if (todoDesc != null && todoDesc.trim().isNotEmpty) {}
+      },
     );
   }
 }
